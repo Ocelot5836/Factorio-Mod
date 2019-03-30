567e7cab58e7e2ddc325ab315aab9a4e17b25297
Ocelot5836 <FinntheRaider@gmail.com>
2019-03-29 20:41:23 -0700

    added notes to transport belt renderer

--- src/main/java/com/ocelot/tileentity/render/RenderTileEntityTransportBelt.java
+++ src/main/java/com/ocelot/tileentity/render/RenderTileEntityTransportBelt.java
@@ -2,28 +2,20 @@ package com.ocelot.tileentity.render;
 
 import com.ocelot.tileentity.belt.TileEntityTransportBelt;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
-import net.minecraft.entity.item.EntityItem;
 
 public class RenderTileEntityTransportBelt extends TileEntityRenderer<TileEntityTransportBelt>
 {
-	private static final EntityItem ITEM = new EntityItem(Minecraft.getInstance().world);
-
 	@Override
 	public void render(TileEntityTransportBelt te, double x, double y, double z, float partialTicks, int destroyStage)
 	{
-		System.out.println("s");
-		ITEM.setWorld(this.getWorld());
-		ITEM.setItem(te.getStackInSlot(3));
-		System.out.println(ITEM.getItem());
+		// TODO find out why this crap doesn't work
+		// System.out.println("s");
 
-		GlStateManager.pushMatrix();
-
-		GlStateManager.translated(x, y, z);
-		Minecraft.getInstance().getRenderManager().renderEntity(ITEM, 0, 0, 0, 0, 0, true);
-
-		GlStateManager.popMatrix();
+		// GlStateManager.pushMatrix();
+		//
+		// GlStateManager.translated(x, y, z);
+		//
+		// GlStateManager.popMatrix();
 	}
 }
