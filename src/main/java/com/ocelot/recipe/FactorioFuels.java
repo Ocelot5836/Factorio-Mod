2fbb369eadb6e56b3170df0ce18009f93ed23f6e
Ocelot5836 <FinntheRaider@gmail.com>
2019-04-04 10:23:36 -0700

    all furnace fuels are now accepted as Factorio Fuels

--- src/main/java/com/ocelot/recipe/FactorioFuels.java
+++ src/main/java/com/ocelot/recipe/FactorioFuels.java
@@ -2,15 +2,14 @@ package com.ocelot.recipe;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.ocelot.FactorioMod;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.IItemProvider;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -23,59 +22,17 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class FactorioFuels
 {
 	/**
-	 * Initializes the joule amounts for each item.
+	 * @return A new map full of the joule amounts
 	 */
 	public static Map<Item, Float> getJouleAmounts()
 	{
 		Map<Item, Float> jouleAmounts = new HashMap<Item, Float>();
 
-		setJoules(jouleAmounts, Blocks.COAL_BLOCK, 40000000);// 40,000,000J
-		setJoules(jouleAmounts, Items.COAL, 4000000);// 4,000,000J
-		setJoules(jouleAmounts, Items.CHARCOAL, 4000000);// 4,000,000J
-		setJoules(jouleAmounts, ItemTags.LOGS, 750000);// 750,000J
-		setJoules(jouleAmounts, ItemTags.PLANKS, 750000);// 750,000J
-		setJoules(jouleAmounts, ItemTags.WOODEN_STAIRS, 750000);// 750,000J
-		setJoules(jouleAmounts, ItemTags.WOODEN_SLABS, 375000);// 375,000J
-		setJoules(jouleAmounts, ItemTags.WOODEN_TRAPDOORS, 750000);// 750,000J
-		setJoules(jouleAmounts, ItemTags.WOODEN_PRESSURE_PLATES, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.OAK_FENCE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.BIRCH_FENCE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.SPRUCE_FENCE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.JUNGLE_FENCE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.DARK_OAK_FENCE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.ACACIA_FENCE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.OAK_FENCE_GATE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.BIRCH_FENCE_GATE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.SPRUCE_FENCE_GATE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.JUNGLE_FENCE_GATE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.DARK_OAK_FENCE_GATE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.ACACIA_FENCE_GATE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.NOTE_BLOCK, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.BOOKSHELF, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.JUKEBOX, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.CHEST, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.TRAPPED_CHEST, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.CRAFTING_TABLE, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.DAYLIGHT_DETECTOR, 750000);// 750,000J
-		setJoules(jouleAmounts, ItemTags.BANNERS, 750000);// 750,000J
-		setJoules(jouleAmounts, Items.BOW, 750000);// 750,000J
-		setJoules(jouleAmounts, Items.FISHING_ROD, 750000);// 750,000J
-		setJoules(jouleAmounts, Blocks.LADDER, 750000);// 750,000J
-		setJoules(jouleAmounts, Items.SIGN, 500000);// 500,000J
-		setJoules(jouleAmounts, Items.WOODEN_SHOVEL, 500000);// 500,000J
-		setJoules(jouleAmounts, Items.WOODEN_SWORD, 500000);// 500,000J
-		setJoules(jouleAmounts, Items.WOODEN_HOE, 500000);// 500,000J
-		setJoules(jouleAmounts, Items.WOODEN_AXE, 500000);// 500,000J
-		setJoules(jouleAmounts, Items.WOODEN_PICKAXE, 500000);// 500,000J
-		setJoules(jouleAmounts, ItemTags.WOODEN_DOORS, 500000);// 500,000J
-		setJoules(jouleAmounts, ItemTags.BOATS, 500000);// 500,000J
-		setJoules(jouleAmounts, ItemTags.WOOL, 250000);// 250,000J
-		setJoules(jouleAmounts, ItemTags.WOODEN_BUTTONS, 250000);// 250,000J
-		setJoules(jouleAmounts, Items.STICK, 250000);// 250,000J
-		setJoules(jouleAmounts, ItemTags.SAPLINGS, 250000);// 250,000J
-		setJoules(jouleAmounts, Items.BOWL, 250000);// 250,000J
-		setJoules(jouleAmounts, ItemTags.CARPETS, 167500);// 167,500J
-		setJoules(jouleAmounts, Blocks.DRIED_KELP_BLOCK, 10002500);// 10,002,500J
+		Map<Item, Integer> burnTimes = TileEntityFurnace.getBurnTimes();
+		for (Entry<Item, Integer> entry : burnTimes.entrySet())
+		{
+			setJoules(jouleAmounts, entry.getKey(), entry.getValue() * 2500);
+		}
 
 		for (Item item : ForgeRegistries.ITEMS)
 		{
@@ -88,6 +45,7 @@ public class FactorioFuels
 		return jouleAmounts;
 	}
 
+	@SuppressWarnings("unused")
 	private static void setJoules(Map<Item, Float> jouleAmounts, Tag<? extends IItemProvider> tag, float joules)
 	{
 		for (IItemProvider provider : tag.getAllElements())
