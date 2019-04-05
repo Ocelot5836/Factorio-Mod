0b89b435f967c84d74b71af9edcb54cbe56cbacb
Ocelot5836 <FinntheRaider@gmail.com>
2019-04-04 23:11:09 -0700

    small bug fixes

--- src/main/java/com/ocelot/event/ClientEvents.java
+++ src/main/java/com/ocelot/event/ClientEvents.java
@@ -2,9 +2,11 @@ package com.ocelot.event;
 
 import org.apache.commons.lang3.tuple.MutablePair;
 
+import com.ocelot.FactorioMod;
 import com.ocelot.recipe.FactorioFuels;
 import com.ocelot.util.EnergyUtils;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
@@ -14,22 +16,22 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ClientEvents
 {
-	private MutablePair<ItemStack, Float> joulesCache = new MutablePair<ItemStack, Float>(ItemStack.EMPTY, 0.0f);
+    private MutablePair<ItemStack, Float> joulesCache = new MutablePair<ItemStack, Float>(ItemStack.EMPTY, 0.0f);
 
-	@SubscribeEvent
-	public void itemTooltipEvent(ItemTooltipEvent event)
-	{
-		ItemStack stack = event.getItemStack();
+    @SubscribeEvent
+    public void itemTooltipEvent(ItemTooltipEvent event)
+    {
+        ItemStack stack = event.getItemStack();
 
-		if (!ItemStack.areItemsEqual(this.joulesCache.getLeft(), stack))
-		{
-			this.joulesCache.setLeft(stack);
-			this.joulesCache.setRight(FactorioFuels.getJoules(stack));
-		}
+        if (!ItemStack.areItemsEqual(this.joulesCache.getLeft(), stack))
+        {
+            this.joulesCache.setLeft(stack);
+            this.joulesCache.setRight(FactorioFuels.getJoules(stack));
+        }
 
-		if (this.joulesCache.getRight() > 0)
-		{
-			event.getToolTip().add(new TextComponentString(EnergyUtils.joulesToString(this.joulesCache.getRight())).setStyle(new Style().setColor(TextFormatting.GRAY)));
-		}
-	}
+        if (this.joulesCache.getRight() > 0)
+        {
+            event.getToolTip().add(new TextComponentString(I18n.format("item." + FactorioMod.MOD_ID + ".tooltip.fuel_value", EnergyUtils.joulesToString(this.joulesCache.getRight()))).setStyle(new Style().setColor(TextFormatting.GRAY)));
+        }
+    }
 }
--- src/main/java/com/ocelot/util/EnumOreType.java
+++ src/main/java/com/ocelot/util/EnumOreType.java
@@ -22,7 +22,7 @@ import net.minecraft.util.IStringSerializable;
  */
 public enum EnumOreType implements IStringSerializable
 {
-    IRON("iron", 1, () -> ModItems.IRON_ORE, () -> ModBlocks.IRON_ORE_OUTCROP.getDefaultState()), COPPER("copper", 1, () -> ModItems.COPPER_ORE, () -> Blocks.GOLD_ORE.getDefaultState()), COAL("coal", 1, () -> Items.COAL, () -> Blocks.COAL_BLOCK.getDefaultState()), STONE("stone", 1, () -> Blocks.STONE, () -> Blocks.STONE.getDefaultState()), URANIUM("uranium", 2, () -> Items.SLIME_BALL, () -> Blocks.SLIME_BLOCK.getDefaultState());
+    IRON("iron", 1, () -> ModItems.IRON_ORE, () -> ModBlocks.IRON_ORE_OUTCROP.getDefaultState()), COPPER("copper", 1, () -> ModItems.COPPER_ORE, () -> ModBlocks.COPPER_ORE_OUTCROP.getDefaultState()), COAL("coal", 1, () -> Items.COAL, () -> Blocks.COAL_BLOCK.getDefaultState()), STONE("stone", 1, () -> Blocks.STONE, () -> Blocks.STONE.getDefaultState()), URANIUM("uranium", 2, () -> Items.SLIME_BALL, () -> Blocks.SLIME_BLOCK.getDefaultState());
 
     private static final Map<String, EnumOreType> NAME_LOOKUP = Maps.<String, EnumOreType>newHashMap();
 
